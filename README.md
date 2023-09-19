888ba6e5f81b85f9a6e0c98c93a439dada900f9e
hqsolomo <hqsolomo@gmail.com>
2023-09-18 22:36:36 -0400

    I promise this is the last commit without a PR

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # StreamTools
 
-This was originally a Java app that I rewrote in Python. The original Java app was written in like 5 minutes and was a simple program to grab a random file based on a macro from touch portal.
+This was originally a Java app that I rewrote in Python. The original Java app was written in like 5 minutes and was a simple program to grab a random file based on a macro from touch portal. This version is one part me practicing with Python, one part upgrading the original, one part getting back into development, and one part "it seemed like a fun project to do".
 
-This version is one part me practicing with Python, one part upgrading the original, one part getting back into development, and one part "it seemed like a fun project to do".
+StreamTools is a set of API commands intended to be usable mostly out-of-the-box but offer more power to those interested in using just the API.
+
+Eventually connecting to multiple storage mediums is the endgoal- allowing users to access clips stored in cloud storage bucket like B2, S3, or MinIO; local filesystem storage; network storage like CIFS or NFS; and services like OneDrive or Google Drive. The ultimate endgame is something that can be hosted in the cloud, pointing to files stored in the cloud.
--- validate_streamtools.py
+++ validate_streamtools.py
@@ -1,15 +1,7 @@
-import os
 import subprocess
 import sys
-import json
-import tkinter as tk
+import api.controlcenter.streamtools_settings as s
 from tkinter import messagebox
-
-
-# Load config file
-config_file = os.path.join(os.path.dirname(__file__), "config.json")
-with open(config_file) as f:
-    config = json.load(f)
 
 
 def install_requirements(requirements_file):
@@ -25,7 +17,7 @@ def install_requirements(requirements_file):
 
 # Check if requirements are installed
 missing_requirements = []
-for requirement in open(config['requirements_file'], 'r').readlines():
+for requirement in open(s.config['requirements_file'], 'r').readlines():
     requirement = requirement.strip()
     try:
         __import__(requirement)
@@ -36,11 +28,11 @@ for requirement in open(config['requirements_file'], 'r').readlines():
 if missing_requirements:
     prompt = f"The following requirements are missing: {', '.join(missing_requirements)}\n\nDo you want to install them now?"
     if messagebox.askyesno("Missing Requirements", prompt):
-        install_requirements(config['requirements_file'])
+        install_requirements(s.config['requirements_file'])
 
         # Check if requirements are installed
         missing_requirements = []
-        for requirement in open(config['requirements_file'], 'r').readlines():
+        for requirement in open(s.config['requirements_file'], 'r').readlines():
             requirement = requirement.strip()
             try:
                 __import__(requirement)
